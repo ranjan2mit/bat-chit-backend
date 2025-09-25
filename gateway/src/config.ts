f5ffb3c039aa5579b5e9539db6b8ae31f32ac73e
Vikash Ranjan <ranjan2mit@gmail.com>
2025-09-25 21:01:02 +0530

    Domain changes for services

--- gateway/src/config.ts
+++ gateway/src/config.ts
@@ -4,4 +4,7 @@ dotenv.config();
 export const config = {
   port: process.env.GATEWAY_PORT || 8000,
   jwtSecret: process.env.JWT_SECRET || 'supersecret',
+  AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL || '',
+  USER_SERVICE_URL: process.env.USER_SERVICE_URL || '',
+  CHAT_SERVICE_URL: process.env.CHAT_SERVICE_URL || '',    
 };
--- gateway/src/routes.ts
+++ gateway/src/routes.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { proxy } from './utils/proxy';
 import { verifyJWT } from './middleware/auth';
+import { config } from './config';
 
 const router = express.Router();
 
 // Public routes (no JWT)
-router.post('/auth/send-otp', proxy('http://auth-service:5001'));
-router.post('/auth/verify-otp', proxy('http://auth-service:5001'));
+router.post('/auth/send-otp', proxy(config.AUTH_SERVICE_URL));
+router.post('/auth/verify-otp', proxy(config.AUTH_SERVICE_URL));
 
 // Protected routes
 router.use('/messages', verifyJWT, proxy('http://message-service:5002'));
